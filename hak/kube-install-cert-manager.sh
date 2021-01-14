a06bcc9277200430959a902cc7f12b7a55645115
Rich Adams <feelingenriched@gmail.com>
2021-01-14 14:40:42 -0800

    Switched to prod letsencrypt

--- hak/kube-install-cert-manager.sh
+++ hak/kube-install-cert-manager.sh
@@ -14,13 +14,13 @@ cat << 'EOF' | kubectl create -f -
 apiVersion: cert-manager.io/v1
 kind: ClusterIssuer
 metadata:
-  name: letsencrypt-staging
+  name: letsencrypt-prod
 spec:
   acme:
     email: feelingenriched@gmail.com
-    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    server: https://acme-v02.api.letsencrypt.org/directory
     privateKeySecretRef:
-      name: letsencrypt-staging
+      name: letsencrypt-prod
     solvers:
       - http01:
           ingress:
