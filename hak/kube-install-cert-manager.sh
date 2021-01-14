36a3207ace55055072ba3a878046c3db0ea973d8
Rich Adams <feelingenriched@gmail.com>
2021-01-14 14:17:12 -0800

    Adding ingress manifests

--- hak/kube-install-cert-manager.sh
+++ hak/kube-install-cert-manager.sh
@@ -9,3 +9,20 @@ fi
 # This is for kubernetes v1.16+
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.crds.yaml
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
+
+cat << 'EOF' | kubectl create -f -
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-staging
+spec:
+  acme:
+    email: feelingenriched@gmail.com
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: letsencrypt-staging
+    solvers:
+      - http01:
+          ingress:
+            class: contour
+EOF
